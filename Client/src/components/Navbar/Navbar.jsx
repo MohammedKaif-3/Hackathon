c242a4f31f4f6e895486891ba98fb829720a407e
Mohammed Kaif <shaikmohammedkaif98@gmail.com>
2025-02-22 14:35:53 +0530

    developing navbar-1

---
+++ Client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Mens</a></li>
+                    <li><a href="#">Kids</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
