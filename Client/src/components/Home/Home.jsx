05b8599fd326cbb37f3b0b838c48df8cd7d79d10
Mohammed Kaif <shaikmohammedkaif98@gmail.com>
2025-02-26 20:29:36 +0530

    ten

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Chat from "../Chat/Chat";
 import "./Home.css";
-
 import Footer from "../Footer/Footer";
 
 const Home = () => {
@@ -10,8 +9,8 @@ const Home = () => {
             <h4>Welcome to the University Collaboration Platform</h4>
             <p>A digital hub for students, faculty and alumini to connect, and collaborate and grow</p>
             <Chat />
-            <img src='https://th.bing.com/th/id/OIP.sE2ufxysUUtJFcFio-3DNAHaE8?rs=1&pid=ImgDetMain'></img>
-           <Footer />
+            <img  id="hero" src='https://th.bing.com/th/id/OIP.sE2ufxysUUtJFcFio-3DNAHaE8?rs=1&pid=ImgDetMain'></img>
+           {/* <Footer /> */}
         </div>
     );
 }
